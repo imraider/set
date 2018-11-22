3956b9de5dbdc4572ad520f70d66772a7031b606
imraider <45240051+imraider@users.noreply.github.com>
2018-11-22 20:05:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  الكلام الي تبيه  `,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`  『LR』♕ .  `,'https://www.twitch.tv/MeeRcY')
 
 });
 
